d8755be9f1c84e93a4e7eab4b3daeb9577ddc8e7
Prithvi23102003 <110010885+Prithvi23102003@users.noreply.github.com>
2022-09-06 19:11:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,9 +33,9 @@ class LinkedList:
             self.head = new_node
         else:
             n = self.head
-            while n.ref is not None:
-                n = n.ref
-            n.ref = new_node
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
 
     def status(self):
         """
